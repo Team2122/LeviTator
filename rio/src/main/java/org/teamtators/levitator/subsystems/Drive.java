625b56d3dd12cbe3e37836f514fd56596d022da6
Abby Chen <chen1049429066@gmail.com>
2018-01-30 21:21:47 -0700

    add drive skeleton (Needs to be worked on still)

---
+++ rio/src/main/java/org/teamtators/levitator/subsystems/Drive.java
@@ -0,0 +1,73 @@
+package org.teamtators.levitator.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PWMSpeedController;
+import org.teamtators.common.hw.ADXRS453;
+import org.teamtators.common.scheduler.Subsystem;
+
+public class Drive extends Subsystem {
+
+    private PWMSpeedController leftMotor;
+    private PWMSpeedController rightMotor;
+    private Encoder rightEncoder;
+    private Encoder leftEncoder;
+    private ADXRS453 gyro;
+    private PIDController rotationController;
+    private PIDController leftController;
+    private PIDController rightController;
+
+    public Drive() {
+        super("Drive");
+    }
+
+    public void driveHeading(double heading, double speed) {
+
+    }
+
+    public void setPowers(double leftPower, double rightPower) {
+
+    }
+
+    public void driveSpeeds(double rightSpeed, double leftSpeed) {
+
+    }
+
+    public void setRightMotorPower(double power) {
+        rightMotor.set(power);
+    }
+
+    public void setLeftMotorPower(double power) {
+        leftMotor.set(power);
+    }
+
+    public double getLeftDistance() {
+        return leftEncoder.getDistance(); //* wheelCircumference;
+    }
+
+
+    public double getRightDistance() {
+        return rightEncoder.getDistance();
+    }
+
+    public double getAverageDistance() {
+        return (getLeftDistance() + getRightDistance())/2.0;
+    }
+
+    public void resetDistance() {
+        leftEncoder.reset();
+        rightEncoder.reset();
+    }
+
+    public double getYawAngle() {
+        return gyro.getAngle();
+    }
+
+    public double getYawRate() {
+        return gyro.getRate();
+    }
+
+    public void resetYawAngle() {
+        gyro.resetAngle();
+    }
+}
