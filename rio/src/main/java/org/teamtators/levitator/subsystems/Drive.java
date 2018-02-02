ecf5d2b364d77523ab33657519cdb90e054f416f
Abby Chen <chen1049429066@gmail.com>
2018-02-01 20:21:15 -0700

    fixed drive

--- rio/src/main/java/org/teamtators/levitator/subsystems/Drive.java
+++ rio/src/main/java/org/teamtators/levitator/subsystems/Drive.java
@@ -6,6 +6,10 @@ import org.teamtators.common.control.PidController;
 import org.teamtators.common.hw.ADXRS453;
 import org.teamtators.common.scheduler.Subsystem;
 
+/**
+ * Has 2 sets of wheels (on the left and right) which can be independently controlled with motors. Also has 2
+ * encoders and a gyroscope to measure the movements of the robot
+ */
 public class Drive extends Subsystem {
 
     private SpeedController leftMotor;
@@ -17,20 +21,67 @@ public class Drive extends Subsystem {
     private PidController leftController;
     private PidController rightController;
 
+    private double speed;
+
     public Drive() {
         super("Drive");
+
+        leftController.setInputProvider(this::getLeftRate);
+        leftController.setOutputConsumer(this::setLeftMotorPower);
+
+        rightController.setInputProvider(this::getRightRate);
+        rightController.setOutputConsumer(this::setRightMotorPower);
+
+        rotationController.setInputProvider(this::getYawAngle);
+        rotationController.setOutputConsumer((double output) -> {
+            setRightMotorPower(speed + output);
+            setLeftMotorPower(speed - output);
+        });
     }
 
+    /**
+     * Drives with a certain heading (angle) and speed
+     *
+     * @param heading in degrees
+     * @param speed   in inches/second
+     */
     public void driveHeading(double heading, double speed) {
+        rightController.stop();
+        leftController.stop();
+        rotationController.start();
+        this.speed = speed;
 
+        rotationController.setSetpoint(heading);
     }
 
-    public void setPowers(double leftPower, double rightPower) {
+    /**
+     * Drives with certain powers
+     *
+     * @param leftPower  the power for the left side
+     * @param rightPower the power for the right side
+     */
+    public void drivePowers(double leftPower, double rightPower) {
+        rightController.stop();
+        leftController.stop();
+        rotationController.stop();
 
+        setRightMotorPower(rightPower);
+        setLeftMotorPower(leftPower);
     }
 
+    /**
+     * Drives with certain speeds
+     *
+     * @param rightSpeed the speed (inches/sec) for the right side
+     * @param leftSpeed  the speed (in/sec) for the left side
+     */
     public void driveSpeeds(double rightSpeed, double leftSpeed) {
+        rotationController.stop();
+        leftController.start();
+        rightController.start();
 
+        leftController.setSetpoint(leftSpeed);
+        rightController.setSetpoint(rightSpeed);
     }
 
     public void setRightMotorPower(double power) {
@@ -39,6 +90,18 @@ public class Drive extends Subsystem {
 
     public void setLeftMotorPower(double power) {
         leftMotor.set(power);
+    }
+
+    public double getLeftRate() {
+        return leftEncoder.getRate();
+    }
+
+    public double getRightRate() {
+        return rightEncoder.getRate();
+    }
+
+    public double getAverageRate() {
+        return (getRightRate() + getLeftRate()) / 2.0;
     }
 
     public double getLeftDistance() {
