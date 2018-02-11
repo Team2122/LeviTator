efdab43027f29531e378922408ce028e5c8f8179
Alex Mikhalev <alexmikhalevalex@gmail.com>
2018-02-11 00:16:17 -0700

    Added pixy integration

--- rio/src/main/java/org/teamtators/levitator/TatorRobot.java
+++ rio/src/main/java/org/teamtators/levitator/TatorRobot.java
@@ -6,6 +6,8 @@ import org.teamtators.common.hw.LogitechF310;
 import org.teamtators.levitator.commands.CommandRegistrar;
 import org.teamtators.levitator.subsystems.Subsystems;
 
+import org.teamtators.pixyjava.*;
+
 public class TatorRobot extends TatorRobotBase {
     private final CommandRegistrar registrar = new CommandRegistrar(this);
 
@@ -15,6 +17,27 @@ public class TatorRobot extends TatorRobotBase {
         super(configDir);
 
         subsystems = new Subsystems();
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+
+        System.loadLibrary("pixyjava");
+
+        int result = PixyUSB.pixy_init();
+        if (result != 0) {
+            logger.error(String.format("pixy_init failed: %d", result));
+            return;
+        } else {
+        }
+        int[] major = new int[1];
+        int[] minor = new int[1];
+        int[] build = new int[1];
+        PixyUSB.pixy_get_firmware_version(major, minor, build);
+        logger.info(String.format("pixy firmware version: %d.%d.%d",
+                major[0], minor[0], build[0]));
+        logger.info("Pixy camera initialized");
     }
 
     public Subsystems getSubsystems() {
