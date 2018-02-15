fcc82224125c88987f062637fa7f38090497b2b3
IsabellaGale <ismg.4102@gmail.com>
2018-02-14 20:46:16 -0700

    add safteies and bumps

--- rio/src/main/java/org/teamtators/levitator/commands/CommandRegistrar.java
+++ rio/src/main/java/org/teamtators/levitator/commands/CommandRegistrar.java
@@ -1,7 +1,9 @@
 package org.teamtators.levitator.commands;
 
 import org.teamtators.common.config.ConfigCommandStore;
+import org.teamtators.common.scheduler.Commands;
 import org.teamtators.levitator.TatorRobot;
+import org.teamtators.levitator.subsystems.Lift;
 
 public class CommandRegistrar {
     private final TatorRobot robot;
@@ -11,13 +13,21 @@ public class CommandRegistrar {
     }
 
     public void register(ConfigCommandStore commandStore) {
+        // Drive commands
         commandStore.registerCommand("DriveTank", () -> new DriveTank(robot));
 
+        // Picker commands
         commandStore.registerCommand("PickerPick", () -> new PickerPick(robot));
         commandStore.registerCommand("PickerRelease", () -> new PickerRelease(robot));
 
+        // Lift commands
+        Lift lift = robot.getSubsystems().getLift();
         commandStore.putCommand("LiftContinuous", new LiftContinuous(robot));
         commandStore.registerCommand("LiftHeightPreset", () -> new LiftHeightPreset(robot));
         commandStore.registerCommand("PivotAnglePreset", () -> new PivotAnglePreset(robot));
+        commandStore.putCommand("BumpLiftUp", Commands.instant(lift::bumpLiftUp));
+        commandStore.putCommand("BumpLiftDown", Commands.instant(lift::bumpLiftDown));
+        commandStore.putCommand("BumpPivotRight", Commands.instant(lift::bumpPivotRight));
+        commandStore.putCommand("BumpPivotLeft", Commands.instant(lift::bumpPivotRight));
     }
 }
