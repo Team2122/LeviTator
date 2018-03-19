f2ef63f7893dfaa75044a8a6c9a06cf46fcb738d
Avery <cattata1056@gmail.com>
2018-03-18 23:07:58 -0600

    Add packet handling

---
+++ slider/src/main/java/SliderPacketType.java
@@ -0,0 +1,5 @@
+public final class SliderPacketType {
+    public static final char PING = 'p';
+    public static final char UPDATE_SLIDER = 's';
+    public static final char RUN_SELF_TEST = 't';
+}
